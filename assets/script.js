eae383a1706838ca30230717aa2baf313c7f9b6e
Chloe Allier <allier.chloe@gmail.com>
2023-09-22 17:25:55 +0200

    Ajout du slide infini et changement classe du point sélectionné

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,9 @@ const slides = [
 
 const arrowLeft = document.querySelector(".arrow_left")
 const arrowRight = document.querySelector(".arrow_right")
+const bannerImage = document.querySelector(".banner_img")
+const bannerTxt = document.querySelector(".banner_txt")
+const dots = document.querySelector(".dots")
 let dot
 let index = 0
 
@@ -40,9 +43,41 @@ addDots()
 arrowLeft.addEventListener("click", goLeft)
 arrowRight.addEventListener("click", goRight)
 
+// Fonction pour aller vers la gauche
 function goLeft() {
-  console.log("Left")
+  if (index > 0) {
+    index--
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`
+    bannerTxt.innerHTML = slides[index].tagLine
+  } else if (index === 0) {
+    index = slides.length - 1
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`
+    bannerTxt.innerHTML = slides[index].tagLine
+  }
+  changeDotClass(index)
 }
+
 function goRight() {
-  console.log("Right")
+  if (index < slides.length - 1) {
+    index++
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`
+    bannerTxt.innerHTML = slides[index].tagLine
+  } else if (index === slides.length - 1) {
+    index = 0
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`
+    bannerTxt.innerHTML = slides[index].tagLine
+  }
+  changeDotClass(index)
+}
+
+// Changer la classe du point sélectionné
+function changeDotClass(index) {
+  let dotActive = document.querySelectorAll(".dot")
+  for (let i = 0; i < slides.length; i++) {
+    if (i === index) {
+      dotActive[i].classList.add("dot_selected")
+    } else {
+      dotActive[i].classList.remove("dot_selected")
+    }
+  }
 }
